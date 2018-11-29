6c05cf3cd879f1df8389027e110cc9093805dba9
martaJesztak <jesztakm@gmail.com>
2018-11-29 10:42:16 +0100

    Movie service added to merge information from OMDB and youtube and send to controller movie information.

---
+++ src/main/java/com/jesztak/movieservice/MovieserviceApplication.java
@@ -0,0 +1,12 @@
+package com.jesztak.movieservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieserviceApplication.class, args);
+    }
+}
--- src/main/java/com/jesztak/movieservice/service/YouTubeService.java
+++ src/main/java/com/jesztak/movieservice/service/YouTubeService.java
@@ -1,7 +1,6 @@
 package com.jesztak.movieservice.service;
 
 import com.jesztak.movieservice.model.Movie;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
@@ -18,26 +17,27 @@ public class YouTubeService {
 
 
     public void addMovieTrailerInfo(Movie movie) {
+        String URI = "https://www.googleapis.com/youtube/v3/search?key=%s&part=%s&maxresults=%s&q=%s";
         String partValue = "snippet";
         int maxresult = 1;
-        String URI = "https://www.googleapis.com/youtube/v3/search?key=%s&part=%s&maxresults=%s&q=%s";
-        String searchURI = String.format(URI,apikey,partValue,maxresult,movie.getTitle());
+        String searchValue = movie.getTitle() + "-movie-trailer";
+        String searchURI = String.format(URI,apikey,partValue,maxresult,searchValue);
+
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
         HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> result = restTemplate.exchange(searchURI, HttpMethod.GET, entity, String.class);
-
         JSONObject jsonObject = new JSONObject(result.getBody());
 
-       JSONArray items = jsonObject.getJSONArray("items");
-       JSONObject item = items.getJSONObject(0);
-       JSONObject id = item.getJSONObject("id");
-       String videoId = id.getString("videoId");
+        String videoId = jsonObject.getJSONArray("items")
+                                    .getJSONObject(0)
+                                    .getJSONObject("id")
+                                    .getString("videoId");
 
-        String videoURI = "https://www.youtube.com/embed/%s?playlist=%s&loop=1";
-        String trailerSource = String.format(videoURI, videoId, videoId);
+        String trailerSource = "https://www.youtube.com/embed/%s?playlist=%s&loop=1";
+        trailerSource = String.format(trailerSource, videoId, videoId);
         movie.setTrailerSource(trailerSource);
     }
 }
